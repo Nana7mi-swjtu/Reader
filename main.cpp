ee2aad0c222b2e35e57cd035dfbce23f2d132466
KaaNoo <zh1186853628@outlook.com>
2025-05-18 22:20:38 +0800

    固定页面大小

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowFlags(w.windowFlags() & ~Qt::WindowMaximizeButtonHint);
+    w.setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
     w.show();
     return a.exec();
 }
